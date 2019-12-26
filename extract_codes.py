243e9249d3a58d03dc10816f87772a88967b2e3f
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2019-12-26 22:52:58 +0800

    + Specity json file

--- extract_codes.py
+++ extract_codes.py
@@ -3,8 +3,11 @@
 __author__ = 'Erimus'
 # extract IR/RF code from Broadlink App
 # put json file and this python in same path, and run it.
+# or with optional argument {json file full path}
+# result will print on console, and save as codes.txt
 
 import os
+import sys
 import json
 import base64
 import binascii
@@ -12,23 +15,30 @@ import binascii
 # ═══════════════════════════════════════════════
 
 
-def main():
-    # find json file (please put only one json in same folder)
-    json_file = None
-    here = os.path.abspath(os.path.dirname(__file__))
-    for path, dirs, files in os.walk(here):  # read all files
-        for fn in files:
-            if fn.endswith('.json'):
-                json_file = os.path.join(path, fn)  # full path
-                print(f'Found Json: {json_file}')
-                break  # root only
-    if not json_file:
-        print('Put the json file (export from sqlite) in same path.')
+def extract_code(json_file=None):
+    # check json file
+    if json_file is None or not json_file.endswith('.json'):
+        # find json file in same path (please put only one json in same folder)
+        json_path = os.path.abspath(os.path.dirname(__file__))
+        for path, dirs, files in os.walk(json_path):  # read all files
+            for fn in files:
+                if fn.endswith('.json'):
+                    json_file = os.path.join(path, fn)  # full path
+                    print(f'Found Json: {json_file}')
+                    break
+            break  # root only
+        if not json_file:
+            print('Put the json file (export from sqlite) in same path.'
+                  'Or input file full path as argument.')
+            return
+    else:  # json file from argument
+        json_path = os.path.abspath(os.path.dirname(json_file))
 
     # read json file
+    print(f'Read Json File: {json_file}\n{"="*30}')
     with open(json_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
-        print(json.dumps(data, ensure_ascii=False, indent=4))
+        # print(json.dumps(data, ensure_ascii=False, indent=4))
 
     # extract
     result = {}
@@ -62,13 +72,15 @@ def main():
     print(json.dumps(result, ensure_ascii=False, indent=4))
 
     # write result file
-    with open(os.path.join(here, 'codes.txt'), 'w', encoding='utf-8') as f:
+    output = os.path.join(json_path, 'codes.txt')
+    with open(output, 'w', encoding='utf-8') as f:
         f.write(json.dumps(result, ensure_ascii=False, indent=2))
-    print('All codes saved in "codes.txt".')
+    print(f'{"="*30}\nAll codes saved in "{output}".')
 
 
 # ═══════════════════════════════════════════════
 
 if __name__ == '__main__':
 
-    main()
+    json_file = sys.argv[1] if len(sys.argv) > 1 else None
+    extract_code(json_file)
